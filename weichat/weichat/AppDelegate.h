12bd9a134c230f54f3bdda2f88a3ef1a691f52b2
LeiShan <hilder@hildertekiiMac.local>
2016-04-21 09:40:46 +0800

    init

---
+++ weichat/weichat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weichat
+//
+//  Created by LeiShan on 16/4/21.
+//  Copyright © 2016年 GongJunNan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weichat/weichat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weichat
+//
+//  Created by LeiShan on 16/4/21.
+//  Copyright © 2016年 GongJunNan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
